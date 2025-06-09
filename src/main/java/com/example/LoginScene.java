0a4adf32e497c92757b89645813b236bad631bb5
marynmendes <marynicolesm@gmail.com>
2025-06-09 17:06:53 -0300

    Cena de login para usuários já cadastrados.

--- src/main/java/com/example/LoginScene.java
+++ src/main/java/com/example/LoginScene.java
@@ -4,11 +4,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -49,10 +47,6 @@ public class LoginScene {
         idLabel.setTextFill(Color.web("#7a7adb"));
         PasswordField idField = new PasswordField();
         idField.setPromptText("********");
-
-        Hyperlink esqueciId = new Hyperlink("Esqueceu seu ID?");
-        esqueciId.setStyle("-fx-text-fill: #7a7adb;");
-        esqueciId.setBorder(Border.EMPTY);
         
         Label loginErroLabel = new Label("Login incorreto!");
         loginErroLabel.setTextFill(Color.RED);
@@ -76,8 +70,8 @@ public class LoginScene {
         }
         if(certo){
             loginErroLabel.setVisible(false);
-            Scene homeScene = MenuScene.menuScene(stage, CadastroParticipanteScene.participanteCadastrado.getNome());
-            stage.setScene(homeScene);
+            Scene menuScene = MenuScene.menuScene(stage, CadastroParticipanteScene.participanteCadastrado.getNome() + "\nID: " + CadastroParticipanteScene.participanteCadastrado.getId());
+            stage.setScene(menuScene);
         } else {
             loginErroLabel.setVisible(true);
         }
